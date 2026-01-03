2fff87b363ef751c3e69850aacd701b853a759d5
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-03 11:37:24 +0530

    variables declaration in js

---
+++ 01-basics/01_variable.js
@@ -0,0 +1,22 @@
+
+
+
+//Employee Details 
+const employeeId=85855;
+let employeeEmail="emp@gmail.com";
+var employeePass="454642";
+employeeLocation = "Delhi";
+let employeeDept;
+
+console.table([employeeId, employeeEmail, employeePass, employeeLocation, employeeDept]);
+
+console.log("After the modifications :");
+
+// const employeeId=123; not allowed constant key word modifications 
+employeeEmail="abc@gmail.com";
+employeePass="12345"
+employeeLocation="Mumbai"
+
+console.log(employeeEmail);
+console.log(employeePass);
+console.log(employeeLocation);
