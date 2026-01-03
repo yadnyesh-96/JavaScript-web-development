2726981cdc7faea3096d4026b349dd6cd74c15b6
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-03 11:41:37 +0530

    data types in js

---
+++ 01-basics/02_data-types.js
@@ -0,0 +1,25 @@
+"use strick";   // trates all js code as a newer version
+
+console.log(3+3)  // code reusability should be high
+
+console.log("yadnyesh");
+
+let name="yadnyesh "
+let age=21;
+let isLogging = false;
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object 
