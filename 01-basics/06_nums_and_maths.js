67e1f1220a1693aaf7c54089cd331a3463445fb7
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-05 17:25:52 +0530

    nusm and maths in js

---
+++ 01-basics/06_nums_and_maths.js
@@ -0,0 +1,18 @@
+
+const score = 10
+console.log(score) //10
+
+
+const balance =new Number(100)
+console.log(balance) //[ Number : 100]
+
+
+const Stringis="ykd"
+console.log(Stringis)
+
+const newString = new String("Yadnyesh")
+console.log(newString)  // [String: 'Yadnyesh']
+
+
+console.log(balance.toString().length) //3
+console.log(balance.toFixed(3)) //100.000
