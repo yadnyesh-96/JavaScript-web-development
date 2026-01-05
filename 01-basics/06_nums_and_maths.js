a4e07e6cfcdffd5a918ec96797ab40c2b00c4d56
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-05 17:37:32 +0530

    Math Function's

--- 01-basics/06_nums_and_maths.js
+++ 01-basics/06_nums_and_maths.js
@@ -45,3 +45,8 @@ console.log(Math.max(4, 3, 6, 8))  //8
 console.log(Math.random())
 console.log((Math.random()*10)+1);
 console.log(Math.floor((Math.random()*10)+1))
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
