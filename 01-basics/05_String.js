c520a86e51241009a5e626c9602e7d0d0155b100
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-05 17:14:01 +0530

    String's in js

--- 01-basics/05_String.js
+++ 01-basics/05_String.js
@@ -13,4 +13,26 @@ console.log(gameName[0]) // a
 
 //console.log(gameName.__proto__)
 
-console.log(gameName.length)
+console.log(gameName.length) //13
+console.log(gameName.toUpperCase()) //ABC@GMAIL.COM
+
+console.log(gameName.charAt(2)) //c
+console.log(gameName.indexOf('@')) //3
+
+const newString=gameName.substring(0,4)
+console.log(newString) //abc@
+
+const newString1=gameName.substring(-8,1)
+console.log(newString1) //a
+
+const newStringOne="  Yadnyesh  "
+console.log(newStringOne) //" Yadnyesh  ""
+console.log(newStringOne.trim()) // "Yadnyesh"
+
+const url="https://yadnyesh.com/yadnyesh%20dhangar"
+
+console.log(url.replace('%20','-')) //https://yadnyesh.com/yadnyesh-dhangar
+
+console.log(url.includes('sundar')) //false
+
+console.log(gameName.split('-')) //[ 'abc@gmail.com' ]
