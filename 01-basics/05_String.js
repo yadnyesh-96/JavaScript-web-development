ec21bac8f4a0931ceb07173afc4031d9a4958ef8
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-05 16:52:16 +0530

    String print types and way's

--- 01-basics/05_String.js
+++ 01-basics/05_String.js
@@ -3,4 +3,6 @@ const name="yadnyesh"
 
 const repoCount = 96
 
-console.log(name + repoCount+" Value")
+//console.log(name + repoCount+" Value")
+
+console.log(`Hello My Name is ${name} and my repo count is ${repoCount}`);
