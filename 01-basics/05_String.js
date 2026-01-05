cf8fe35b404dda131afefc3608f02351fe88a2f0
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-05 17:06:14 +0530

    String accesing and methods

--- 01-basics/05_String.js
+++ 01-basics/05_String.js
@@ -6,3 +6,11 @@ const repoCount = 96
 //console.log(name + repoCount+" Value")
 
 console.log(`Hello My Name is ${name} and my repo count is ${repoCount}`);
+
+const gameName =new String('abc@gmail.com')
+
+console.log(gameName[0]) // a
+
+//console.log(gameName.__proto__)
+
+console.log(gameName.length)
