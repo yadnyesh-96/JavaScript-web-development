b0be6c6ca8f0a895805b759624f44aef78c1bac3
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-18 17:41:58 +0530

    Practice on Array in javaScript

---
+++ 02-basics/01-Array's/02-array.js
@@ -0,0 +1,17 @@
+let n = [1, 2, 3, 4]
+
+console.log(n)
+console.log("--------------")
+
+for(let i=0; i<n.length; i++){
+    console.log(n[i]," ");
+}
+
+console.log("--------------")
+
+let k=0;
+while(k<n.length){
+    console.log(n[k]+" ")
+    k++;
+}
+
