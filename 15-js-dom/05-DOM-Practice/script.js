3c8702a8583fb38a4b33cdb02feda6084ee4691b
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-20 22:59:28 +0530

    build calculator using js

--- 15-js-dom/05-DOM-Practice/script.js
+++ 15-js-dom/05-DOM-Practice/script.js
@@ -1,4 +1,5 @@
 
+
 function calculatorOperation(){
     let btns = [
         ['1','2','3','+'],
@@ -9,7 +10,7 @@ function calculatorOperation(){
 
     // let firstValue,secondValue,result;
     
-    let bntArea = document.getElementById("button-sec");
+    let bntArea = document.querySelector(".button-sec"); 
 
     for(let i=0; i<btns.length; i++){
         for(let j=0; j<btns[i].length; j++){
@@ -17,8 +18,9 @@ function calculatorOperation(){
             let btn = document.createElement("input");
             btn.setAttribute("type","button");
             btn.setAttribute("value",`${btns[i][j]}`)
-            btn.setAttribute("class","button-sec")
-            
+            btn.setAttribute("class","num-btn")
+
+            btn.style.color = "#000"
             bntArea.appendChild(btn)
         }
     }
