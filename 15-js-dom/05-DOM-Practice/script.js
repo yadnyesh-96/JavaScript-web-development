c9882a2049276b4757997cb289da6b828070f9f9
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-20 23:57:31 +0530

    build calculator using js

--- 15-js-dom/05-DOM-Practice/script.js
+++ 15-js-dom/05-DOM-Practice/script.js
@@ -1,28 +1,63 @@
 
+let firstValue = "";
+let choice = "";
 
-function calculatorOperation(){
+function calculatorOperation() {
     let btns = [
-        ['1','2','3','+'],
-        ['4','5','6','-'],
-        ['7','8','9','*'],
-        ['0','.','=','/']
+        ['1', '2', '3', '+'],
+        ['4', '5', '6', '-'],
+        ['7', '8', '9', '*'],
+        ['0', '.', '=', '/']
     ];
 
-    // let firstValue,secondValue,result;
-    
-    let bntArea = document.getElementById("button-sec"); 
 
-    for(let i=0; i<btns.length; i++){
-        for(let j=0; j<btns[i].length; j++){
+
+    let bntArea = document.getElementById("button-sec");
+
+    for (let i = 0; i < btns.length; i++) {
+        for (let j = 0; j < btns[i].length; j++) {
 
             let btn = document.createElement("input");
-            btn.setAttribute("type","button");
-            btn.setAttribute("value",`${btns[i][j]}`)
-            btn.setAttribute("class","num-btn")
+            btn.setAttribute("type", "button");
+            btn.setAttribute("value", `${btns[i][j]}`)
+            btn.setAttribute("class", "num-btn")
 
             btn.style.color = "#282828"
+
+            btn.addEventListener("click", () => {
+                let inputSec = document.getElementById("val-inp");
+                let opVal = btns[i][j];
+
+                if (opVal === '+' || opVal === '-' || opVal === '*' || opVal === '/') {
+                    firstValue = inputSec.value;
+                    choice = opVal;
+                    inputSec.value = "";
+                } else if (opVal === '=') {
+                    let secondValue = inputSec.value;
+                    let result;
+
+                    let num1 = parseFloat(firstValue);
+                    let num2 = parseFloat(secondValue);
+
+                    switch (choice) {
+                        case '+': result = num1 + num2; break;
+                        case '-': result = num1 - num2; break;
+                        case '*': result = num1 * num2; break;
+                        case '/': result = num2 !== 0 ? num1 / num2 : "Error"; break;
+                    }
+                    inputSec.value = result;
+                    firstValue = "";
+                } else {
+                    inputSec.value += opVal;
+                }
+
+            });
+
             bntArea.appendChild(btn)
+
         }
     }
+
+
 
 }
