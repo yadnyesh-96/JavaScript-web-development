34dc7085630778e1194b8c488432d918f6b525b6
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-21 16:33:09 +0530

    Simple Image Slider

--- 15-js-dom/07-Image-Slider/script.js
+++ 15-js-dom/07-Image-Slider/script.js
@@ -1,1 +1,23 @@
+let imgStorage = [
+    "../07-Image-Slider/img-1.jpg",
+    "../07-Image-Slider/img-2.jpg",
+    "../07-Image-Slider/img-3.jpg",
+    "../07-Image-Slider/img-4.jpg",
+    "../07-Image-Slider/img-5.jpg",
+    "../07-Image-Slider/img-6.jpg"
 
+]
+
+// let imgEle = document.getElementById("img-data")
+
+let index=0;
+console.log(imgStorage[index])
+
+
+function nextImage() {
+    imgEle.src = "../07-Image-Slider/img-1.jpg"
+}
+
+function prevImage() {
+
+}
