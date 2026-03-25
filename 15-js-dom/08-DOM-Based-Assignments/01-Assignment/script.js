76d99f358dc9ce1a78ee3227dd0092a2f10bf872
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-25 21:21:49 +0530

    add text project

---
+++ 15-js-dom/08-DOM-Based-Assignments/01-Assignment/script.js
@@ -0,0 +1,12 @@
+
+
+let infodiv = document.getElementById("s2")
+
+let pTag = document.createElement("p");
+pTag.innerHTML = "Agriculture remains the backbone of India, and scenes like this highlight its importance.The neatly arranged fields show careful planning and hard work by farmers. Such landscapes also reflect India’s deep connection with nature and tradition. Villages like these preserve cultural values and simple ways of living.The greenery provides not just food but also a sense of peace and well - being. The riverbanks lined with trees offer shelter to birds and wildlife. This creates a balanced ecosystem where humans and nature coexist. Overall, the image is a reminder of India’s rural beauty and agricultural strength. It encourages us to appreciate the simplicity and sustainability of village life.In a fast - changing world, such scenes inspire us to stay connected to our roots."
+
+// infodiv.appendChild(pTag)
+
+function addContent(){
+    infodiv.appendChild(pTag);
+}
