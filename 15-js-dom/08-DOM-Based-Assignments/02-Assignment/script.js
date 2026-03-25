f60287976aa2e8eb22ba579e0c3740d5e4ae7701
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-25 21:30:27 +0530

    baground changer using dom

--- 15-js-dom/08-DOM-Based-Assignments/02-Assignment/script.js
+++ 15-js-dom/08-DOM-Based-Assignments/02-Assignment/script.js
@@ -1,2 +1,29 @@
 const bodyElement = document.querySelector('body');
 
+function colorOne(){
+    bodyElement.style.background = '#EAFF7B'
+}
+
+function colorTwo(){
+    bodyElement.style.background = '#233A77'
+}
+
+function colorThree(){
+    bodyElement.style.background = '#29BDC1'
+}
+
+function colorFour(){
+    bodyElement.style.background = '#D84242'
+}
+
+function colorFive(){
+    bodyElement.style.background = '#913F92'
+}
+
+function colorSix(){
+    bodyElement.style.background = '#75420E'
+}
+
+function colorSeven(){
+    bodyElement.style.background = '#AAAAAA'
+}
