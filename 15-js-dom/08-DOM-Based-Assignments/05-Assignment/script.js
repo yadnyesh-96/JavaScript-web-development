8df9605b921999c1b179d627b5b6eb5070ed10ee
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-26 09:53:53 +0530

    counter

--- 15-js-dom/08-DOM-Based-Assignments/05-Assignment/script.js
+++ 15-js-dom/08-DOM-Based-Assignments/05-Assignment/script.js
@@ -1,3 +1,11 @@
+let countBtn = document.getElementById("countButton");
 
-let count = document.getElementById("count");
-let 
+let countText = document.getElementById("count");
+let count = 1;
+let counter = document.createElement('h1');
+    counter.setAttribute(id,'count');
+    
+function myCount(){
+    counter.innerHTML = count;
+    countText
+}
