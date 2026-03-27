025fa18ceba8d8e56b5df829e1efb42cf6fefff8
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-27 23:52:35 +0530

    To do List

--- 15-js-dom/08-DOM-Based-Assignments/08-Assignment/script.js
+++ 15-js-dom/08-DOM-Based-Assignments/08-Assignment/script.js
@@ -1,1 +1,21 @@
 
+let count = 0;
+function addMyTask(){
+
+    let dataInput = document.getElementById("taskInp");
+    let InputData = dataInput.value;
+
+    let trEle = document.createElement("tr");
+    let tdEle1 = document.createElement("td");
+    let tdEle2 = document.createElement("td");
+    let tdEle3 = document.createElement("td");
+    let tdEle4 = document.createElement("td");
+
+
+    tdEle1.innerHTML=count+1;
+    tdEle2.innerHTML=InputData;
+    tdEle3.innerHTML='<button class="MarkTask"><i class="fa-solid fa-list-check"></i>';
+    tdEle4.innerHTML='<button class="MarkTask"><i class="fa-solid fa-list-check"></i>';
+
+
+}
