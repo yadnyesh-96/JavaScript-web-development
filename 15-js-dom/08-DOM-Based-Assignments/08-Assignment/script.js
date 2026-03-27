e945af7b54b706c8908408672cb60cb5074db232
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-28 00:00:56 +0530

    To do List

--- 15-js-dom/08-DOM-Based-Assignments/08-Assignment/script.js
+++ 15-js-dom/08-DOM-Based-Assignments/08-Assignment/script.js
@@ -1,4 +1,4 @@
-
+let t = document.getElementById("listTable")
 let count = 0;
 function addMyTask(){
 
@@ -11,11 +11,17 @@ function addMyTask(){
     let tdEle3 = document.createElement("td");
     let tdEle4 = document.createElement("td");
 
-
-    tdEle1.innerHTML=count+1;
+    count+=1;
+    tdEle1.innerHTML=count;
     tdEle2.innerHTML=InputData;
     tdEle3.innerHTML='<button class="MarkTask"><i class="fa-solid fa-list-check"></i>';
-    tdEle4.innerHTML='<button class="MarkTask"><i class="fa-solid fa-list-check"></i>';
+    tdEle4.innerHTML='<button class="MarkTask1"><i class="fa-solid fa-trash"></i></button>';
 
+    trEle.appendChild(tdEle1)
+    trEle.appendChild(tdEle2)
+    trEle.appendChild(tdEle3)
+    trEle.appendChild(tdEle4)
 
+    t.appendChild(trEle)
 }
+
