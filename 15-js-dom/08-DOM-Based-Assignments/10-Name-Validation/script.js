bb42c08a1bf855dff0adae40ad074480ac807de5
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-28 11:45:46 +0530

    basic name validations

---
+++ 15-js-dom/08-DOM-Based-Assignments/10-Name-Validation/script.js
@@ -0,0 +1,11 @@
+
+let cardId = document.getElementById("card");
+
+let userInput = document.getElementById("userInput");
+let passInput = document.getElementById("passInput");
+
+const logBtn = document.getElementById("LoginBtn");
+
+logBtn.addEventListener('click',()=>{
+    alert("Hello")
+})
