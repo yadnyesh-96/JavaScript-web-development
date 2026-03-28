85b54f2aaef9ec10b24b30ee346f7badcb565b3e
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-28 18:15:16 +0530

    basic name validations

--- 15-js-dom/08-DOM-Based-Assignments/09-pop-card/script.js
+++ 15-js-dom/08-DOM-Based-Assignments/09-pop-card/script.js
@@ -1,11 +1,12 @@
 
+setTimeout(
 function visiblePop() {
     let circleBox = document.getElementById("circle");
     let cardBox = document.getElementById("card");
 
     circleBox.style.visibility='visible';
     cardBox.style.visibility='visible';
-}
+},900)
 
 function closePop(){
     let circleBox = document.getElementById("circle");
--- 15-js-dom/08-DOM-Based-Assignments/10-Name-Validation/script.js
+++ 15-js-dom/08-DOM-Based-Assignments/10-Name-Validation/script.js
@@ -8,21 +8,59 @@ const userInput = document.getElementById("userInput");
 userInput.addEventListener('keyup', () => {
     let str = userInput.value;
     let flag = false;
+    let charCount=false;
+    
     for (let i = 0; i < str.length; i++) {
         let chars = str.charCodeAt(i);
         if (!((chars >= 65 && chars <= 90) || (chars >= 97 && chars <= 122))) {
             flag = true;
-            break;
-        }
-        if(flag){
-            cardId.style.borderColor='red'
-        }else{
-            cardId.style.borderColor='green'
+
+            if(chars>=48 || chars<=57){
+                charCount=true;
+                break;
+            }else{
+                charCount=false;
+            }
+            break; 
         }
     }
-    
 
+    let warn = document.createElement("h2");
+    warn.setAttribute("id", "warn");
+    warn.innerHTML = "Invalid username only capital and small allowed"
+
+    const myWarn = document.getElementById("warn");
+
+    if(charCount){
+        userInput.insertAdjacentElement("afterend", warn)
+    }else{
+        myWarn.remove()
+    }
+
+    if (str.length == 0) {
+        userInput.style.boxShadow = "none"
+        myWarn.remove()
+    } else if (flag) {
+        userInput.style.boxShadow = "0 0 5px red";
+        myWarn.remove()
+    } else {
+        userInput.style.boxShadow = "0 0 5px green";
+        myWarn.remove()
+    }
 });
+
+const upassInput = document.getElementById("passInput");
+upassInput.addEventListener('keyup', () => {
+    let passLength = upassInput.value
+    if (passLength.length == 0) {
+        upassInput.style.boxShadow = "none"
+    } else if (passLength.length >= 6) {
+        upassInput.style.boxShadow = "0 0 5px green";
+    } else {
+        upassInput.style.boxShadow = "0 0 5px red";
+    }
+})
+
 
 logBtn.addEventListener('click', () => {
     let passInput = document.getElementById("passInput").value;
