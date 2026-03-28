3e260db94683a962fc86fa0fd3c686cfb920fe0d
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-28 12:09:11 +0530

    basic name validations

--- 15-js-dom/08-DOM-Based-Assignments/10-Name-Validation/script.js
+++ 15-js-dom/08-DOM-Based-Assignments/10-Name-Validation/script.js
@@ -1,11 +1,30 @@
 
-let cardId = document.getElementById("card");
-
-let userInput = document.getElementById("userInput");
-let passInput = document.getElementById("passInput");
+const cardId = document.getElementById("card");
 
 const logBtn = document.getElementById("LoginBtn");
 
-logBtn.addEventListener('click',()=>{
-    alert("Hello")
-})
+
+const userInput = document.getElementById("userInput");
+userInput.addEventListener('keyup', () => {
+    let str = userInput.value;
+    let flag = false;
+    for (let i = 0; i < str.length; i++) {
+        let chars = str.charCodeAt(i);
+        if (!((chars >= 65 && chars <= 90) || (chars >= 97 && chars <= 122))) {
+            flag = true;
+            break;
+        }
+        if(flag){
+            cardId.style.borderColor='red'
+        }else{
+            cardId.style.borderColor='green'
+        }
+    }
+    
+
+});
+
+logBtn.addEventListener('click', () => {
+    let passInput = document.getElementById("passInput").value;
+    console.log("Logging in with:", userInput.value, passInput);
+});
