94d3ad5a394bb22cec14db30ec70f68e750046ee
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-28 11:03:20 +0530

    pop card

---
+++ 15-js-dom/08-DOM-Based-Assignments/09-Assignment/script.js
@@ -0,0 +1,16 @@
+
+function visiblePop() {
+    let circleBox = document.getElementById("circle");
+    let cardBox = document.getElementById("card");
+
+    circleBox.style.visibility='visible';
+    cardBox.style.visibility='visible';
+}
+
+function closePop(){
+    let circleBox = document.getElementById("circle");
+    let cardBox = document.getElementById("card");
+
+    circleBox.style.visibility='hidden';
+    cardBox.style.visibility='hidden';
+}
