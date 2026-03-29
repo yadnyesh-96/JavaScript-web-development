ae6420a3a2a0477868dc90bb284a5e07f2ee500a
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-30 00:14:24 +0530

    driving licens validation

---
+++ 15-js-dom/08-DOM-Based-Assignments/12-aadharCard-validation'/script.js
@@ -0,0 +1,38 @@
+
+const myInput = document.getElementById('myInput');
+const msg = document.getElementById('msg');
+
+
+
+myInput.addEventListener('input', () => {
+    let num = myInput.value
+    if (num.length > 12) {
+        msg.style.color = 'red'
+        myInput.style.boxShadow = '0 0 6px red'
+        msg.innerHTML = "Invalid aadhar number must be 12 digits"
+
+    } else {
+        let flag = false;
+
+        for (let i = 0; i < num.length; i++) {
+
+            let ch = num.charCodeAt(i);
+            if (ch >= 48 || ch <= 57) {
+                flag = true;
+                break;
+            }
+            
+        }
+        if (flag) {
+            msg.style.color = 'red'
+            myInput.style.boxShadow = '0 0 6px red'
+            msg.innerHTML = "Invalid aadhar number must be 12 digits"
+        } else {
+            msg.style.color = 'green'
+            myInput.style.boxShadow = '0 0 6px green'
+            msg.innerHTML = "valid aadhar number"
+        }
+    }
+
+
+})
