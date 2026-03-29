dbcd49d74c7daedec2e64fd0e1769d4464b29225
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-30 01:04:00 +0530

    mobile no validation

---
+++ 15-js-dom/08-DOM-Based-Assignments/13-mobile-no-validation/script.js
@@ -0,0 +1,47 @@
+
+const myInput = document.getElementById('myInput');
+const msg = document.getElementById('msg');
+
+
+
+myInput.addEventListener('input', () => {
+    let num = myInput.value
+    if (num.length > 12) {
+        msg.style.color = 'red'
+        myInput.style.boxShadow = '0 0 6px red'
+        msg.innerHTML = "Invalid mobile number must be 10 digits"
+
+    } 
+    
+    
+    if(num.length==12){
+        let flag = false;
+
+        for (let i = 0; i < num.length; i++) {
+
+            let ch = num.charCodeAt(i);
+            if (ch >= 48 || ch <= 57) {
+                flag = true;
+                break;
+            }
+
+        }
+        if (!flag) {
+            msg.style.color = 'red'
+            myInput.style.boxShadow = '0 0 6px red'
+            msg.innerHTML = "Invalid mobile number must be 10 digits"
+        } else {
+            msg.style.color = 'green'
+            myInput.style.boxShadow = '0 0 6px green'
+            msg.innerHTML = "valid mobile number"
+        }
+    }
+
+    if(num == " "){
+         msg.style.color = 'none'
+            myInput.style.boxShadow = 'none'
+            msg.innerHTML = ""
+    }
+
+
+})
